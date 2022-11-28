a20423ca4306ff4b34e4ee13dc66d5aaabebbe5d
Piotr Brudny <brudny.piotr@gmail.com>
2022-11-28 12:49:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# wdlformatter README
+
+## Features
+
+Allows upgrade and format WDL files.
+
+## Requirements
+
+You need to set download wdlTools.jar executable file from https://github.com/dnanexus/wdlTools/releases
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `WDL.formatter.wdlTools.location`: You must specify wdlTools.jar executable file location.
+* `WDL.formatter.enable`: Enable/disable this extension.
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+### 0.0.1
+
+Initial release of ...
+
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,112 @@
+'use strict';
+
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as child_process from "child_process";
+import { stringify } from 'querystring';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	console.log('WDL formatting activated');
+
+	vscode.commands.registerCommand('WDL.formatter.upgrade', () => {
+		const { activeTextEditor } = vscode.window;
+
+		if (activeTextEditor && activeTextEditor.document.languageId === 'wdl') {
+			const { document } = activeTextEditor;
+			const firstLine = document.lineAt(0);
+			const lastLine = document.lineAt(document.lineCount - 1);
+
+			const allTextRange = new vscode.Range(
+				0,
+				firstLine.range.start.character,
+				document.lineCount - 1,
+				lastLine.range.end.character
+			);
+
+			const wdlToolsLocation = vscode.workspace.getConfiguration('WDL.formatter.wdlTools').get('location');
+
+			try {
+				let out = child_process.execSync('java -jar ' + wdlToolsLocation + ' upgrade --nofollow-imports ' + document.uri.path);
+
+				const edit = new vscode.WorkspaceEdit();
+				edit.replace(document.uri, allTextRange, out.toString());
+				return vscode.workspace.applyEdit(edit);
+
+			} catch (error) {
+				reportErrorNoUpgrade({ message: getErrorMessage(error) });
+			}
+		}
+	});
+
+	vscode.languages.registerDocumentFormattingEditProvider('wdl', {
+		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+			const firstLine = document.lineAt(0);
+			const lastLine = document.lineAt(document.lineCount - 1);
+
+			const allTextRange = new vscode.Range(
+				0,
+				firstLine.range.start.character,
+				document.lineCount - 1,
+				lastLine.range.end.character
+			);
+
+			const wdlToolsLocation = vscode.workspace.getConfiguration('WDL.formatter.wdlTools').get('location');
+
+			try {
+				let out = child_process.execSync('java -jar ' + wdlToolsLocation + ' format --nofollow-imports ' + document.uri.path);
+
+				// let commandRegExp = new RegExp('.*command.*<<<\W+([\w\W]+)>>>');
+				// let matches = commandRegExp.exec(out.toString());
+				// console.log("supa watch22s");
+
+				// while (matches !== null) {
+				// 	console.log(matches[1]);
+				// }
+
+				return [vscode.TextEdit.replace(allTextRange, out.toString())];
+			} catch (error) {
+				let message = getErrorMessage(error);
+
+				if (message.includes('WDL version Draft_2 is not supported')) {
+					reportError({ message }).then(result => {
+						if (result) {
+							vscode.commands.executeCommand('WDL.formatter.upgrade');
+						}
+					});
+				} else {
+					reportErrorNoUpgrade({ message });
+				}
+
+				return [];
+			}
+		}
+	});
+
+}
+
+const reportError = ({ message }: { message: string }) => {
+	return vscode.window.showErrorMessage(message, 'Perform file upgrade');
+};
+
+const reportErrorNoUpgrade = ({ message }: { message: string }) => {
+	vscode.window.showErrorMessage(message);
+};
+
+function getErrorMessage(error: unknown) {
+	let message = 'Unknown Error';
+	if (error instanceof Error) {
+		message = 'Library formatting error: ';
+		let details = error.message.split("\n")[2];
+		message += details;
+	}
+
+	return message;
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {
+	console.log('WDL formatting deactivated');
+}
