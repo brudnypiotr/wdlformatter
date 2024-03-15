9132c138b4ca0d9727e665383d1dbb5cb0ebf90f
Piotr Brudny <brudny.piotr@gmail.com>
2024-03-16 00:01:24 +0100

    Refactor

--- src/extension.ts
+++ src/extension.ts
@@ -6,60 +6,16 @@ const FIXER_RB_CONFIG = 'WDL.formatter.fix.rubyRb';
 const WDL_LANGUAGE_ID = 'wdl';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('WDL formatting activated');
-
     vscode.commands.registerCommand('WDL.formatter.fix.ruby', () => {
-        const activeEditor = vscode.window.activeTextEditor;
-        if (!activeEditor || activeEditor.document.languageId !== WDL_LANGUAGE_ID) {
-            return;
-        }
-
-        const document = activeEditor.document;
-        const fixerRbLocation = vscode.workspace.getConfiguration(FIXER_RB_CONFIG).get('location');
-
-        const outputPath = `${document.uri.fsPath}_`;
-        child_process.execSync(`${fixerRbLocation} "${document.uri.fsPath}" > "${outputPath}"`);
-        child_process.execSync(`mv "${outputPath}" "${document.uri.fsPath}"`);
+        executeCommandForWdlFile(fixRubyCommand);
     });
 
     vscode.commands.registerCommand('WDL.formatter.check', () => {
-        const activeEditor = vscode.window.activeTextEditor;
-        if (!activeEditor || activeEditor.document.languageId !== WDL_LANGUAGE_ID) {
-            return;
-        }
-
-        const document = activeEditor.document;
-        const wdlToolsJarLocation = vscode.workspace.getConfiguration(WDL_TOOLS_CONFIG).get('location');
-
-        const outputChannel = vscode.window.createOutputChannel("WDL Formatter");
-        outputChannel.show();
-
-        try {
-            const out = child_process.execSync(`java -jar ${wdlToolsJarLocation} check "${document.uri.fsPath}"`);
-            outputChannel.append(out.toString());
-        } catch (error) {
-            handleError(error, outputChannel);
-        }
+        executeCommandForWdlFile(checkCommand);
     });
 
     vscode.commands.registerCommand('WDL.formatter.upgrade', () => {
-        const activeEditor = vscode.window.activeTextEditor;
-        if (!activeEditor || activeEditor.document.languageId !== WDL_LANGUAGE_ID) {
-            return;
-        }
-
-        const document = activeEditor.document;
-        const wholeDocumentRange = getWholeDocumentRange(document);
-        const wdlToolsJarLocation = vscode.workspace.getConfiguration(WDL_TOOLS_CONFIG).get('location');
-
-        try {
-            const out = child_process.execSync(`java -jar ${wdlToolsJarLocation} upgrade --nofollow-imports "${document.uri.fsPath}"`);
-            const edit = new vscode.WorkspaceEdit();
-            edit.replace(document.uri, wholeDocumentRange, out.toString());
-            vscode.workspace.applyEdit(edit);
-        } catch (error) {
-            handleError(error);
-        }
+        executeCommandForWdlFile(upgradeCommand);
     });
 
     vscode.languages.registerDocumentFormattingEditProvider('wdl', {
@@ -67,21 +23,66 @@ export function activate(context: vscode.ExtensionContext) {
     });
 }
 
+function executeCommandForWdlFile(command: (uri: vscode.Uri) => void) {
+    const activeEditor = vscode.window.activeTextEditor;
+    if (!activeEditor || activeEditor.document.languageId !== WDL_LANGUAGE_ID) {
+        return;
+    }
+
+    command(activeEditor.document.uri);
+}
+
+function fixRubyCommand(uri: vscode.Uri) {
+    const document = vscode.workspace.textDocuments.find(doc => doc.uri === uri);
+    if (!document) {
+        return;
+    }
+
+    const fixerRbLocation = vscode.workspace.getConfiguration(FIXER_RB_CONFIG).get('location');
+    const outputPath = `${uri.fsPath}_`;
+    child_process.execSync(`${fixerRbLocation} "${uri.fsPath}" > "${outputPath}"`);
+    child_process.execSync(`mv "${outputPath}" "${uri.fsPath}"`);
+}
+
+function checkCommand(uri: vscode.Uri) {
+    const wdlToolsJarLocation = vscode.workspace.getConfiguration(WDL_TOOLS_CONFIG).get('location');
+    const outputChannel = vscode.window.createOutputChannel("WDL Formatter");
+    outputChannel.show();
+
+    try {
+        const out = child_process.execSync(`java -jar ${wdlToolsJarLocation} check "${uri.fsPath}"`);
+        outputChannel.append(out.toString());
+    } catch (error) {
+        handleCommandError(error);
+    }
+}
+
+function upgradeCommand(uri: vscode.Uri) {
+    const wdlToolsJarLocation = vscode.workspace.getConfiguration(WDL_TOOLS_CONFIG).get('location');
+    try {
+        const out = child_process.execSync(`java -jar ${wdlToolsJarLocation} upgrade --nofollow-imports "${uri.fsPath}"`);
+        const edit = new vscode.WorkspaceEdit();
+        edit.replace(uri, getWholeDocumentRange(uri), out.toString());
+        vscode.workspace.applyEdit(edit);
+    } catch (error) {
+        handleCommandError(error);
+    }
+}
+
 function formatWdlDocument(document: vscode.TextDocument): vscode.TextEdit[] {
     const wdlToolsLocation = vscode.workspace.getConfiguration('WDL.formatter.wdlTools').get('location');
     try {
         let out = child_process.execSync(`java -jar ${wdlToolsLocation} format --nofollow-imports "${document.uri.fsPath}"`).toString();
         out = formatBashCommand(out);
-        return [vscode.TextEdit.replace(getWholeDocumentRange(document), out)];
+        return [vscode.TextEdit.replace(getWholeDocumentRange(document.uri), out)];
     } catch (error) {
-        handleError(error);
+        handleCommandError(error);
         return [];
     }
 }
 
-
 function formatBashCommand(output: string): string {
-    const commandRegExp = new RegExp('([ \t]*)command +<{3}([^]+)>{3}', 'mg');
+	const commandRegExp = new RegExp('([ \t]*)command +<{3}([^]+)>{3}', 'mg');
     const bashCommand = commandRegExp.exec(output);
 
     if (bashCommand) {
@@ -95,18 +96,19 @@ function formatBashCommand(output: string): string {
     return output;
 }
 
-function getWholeDocumentRange(document: vscode.TextDocument) {
+function getWholeDocumentRange(uri: vscode.Uri): vscode.Range {
+    const document = vscode.workspace.textDocuments.find(doc => doc.uri === uri);
+    if (!document) {
+        return new vscode.Range(0, 0, 0, 0);
+    }
     const firstLine = document.lineAt(0);
     const lastLine = document.lineAt(document.lineCount - 1);
     return new vscode.Range(firstLine.range.start, lastLine.range.end);
 }
 
-function handleError(error: unknown, outputChannel?: vscode.OutputChannel) {
+function handleCommandError(error: unknown) {
     const message = getErrorMessage(error);
     vscode.window.showErrorMessage(message);
-    if (outputChannel && error instanceof Error) {
-        outputChannel.appendLine(error.message);
-    }
 }
 
 function getErrorMessage(error: unknown): string {
